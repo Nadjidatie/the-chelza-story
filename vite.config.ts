d6dca2cf72cd1ee1a5d2464c787d22f888d2a128
nadjidah <houssouny-abdallah.nadjidatie@etu.univ-grenoble-alpes.fr>
2025-12-21 22:29:03 +0100

    modif

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // très important pour GitHub Pages
-  base: '/the-chelza-story/',
-  build: {
-    outDir: 'docs', // on build dans docs/ car GitHub Pages pointe dessus
-  },
 })
